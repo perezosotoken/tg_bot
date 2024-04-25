f01c67276cacf6ce12bd154173afd718362c137f
alberthaptic <albert@haptic.finance>
2024-04-25 17:13:37 -0400

    tg bot WIP

--- server/main.js
+++ server/main.js
@@ -1,22 +1,40 @@
-require('dotenv').config()
+require('dotenv').config();
+const fs = require('fs');
 
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
+const sendToken = require('./token');
 
 const app = express();
 
 const { 
   expressApp, 
   lastCmd, 
-  userAuthData, 
   userStates, 
-  usernames, 
-  depositAddresses, 
   setLastCmd, 
   corsOptions 
 } = require('./server');
 
-const { generateMultipleCodes, saveCodesToJsonFile, searchForCode } = require('../utils.js');
+const { generateMultipleCodes, saveCodesToJsonFile, searchForCode, removeCodeFromList, populateAuthData } = require('../utils.js');
+const { parseEther } = require('ethers');
+let userAuthData ;
+
+let depositAddresses, usernames = [];
+
+try {
+    (async () => {
+    userAuthData = await populateAuthData();
+
+    const data = fs.readFileSync('data/appData.json', 'utf8');
+    depositAddresses = JSON.parse(data);
+    // Extract the usernames by splitting the keys
+    usernames = Object.keys(depositAddresses).map(key => key.split(':')[0]);
+    })()
+  } catch (err) {
+    console.error("Error reading or parsing the file", err);
+    depositAddresses = {}; // fallback to empty object
+}
+ 
 
 // Use the Express application from the module
 app.use(expressApp);
@@ -46,211 +64,156 @@ function updateIdleState(username) {
   return true;
 }
 
-// Match lastCmd command and return lastCmd
-bot.onText(/\/cmd (.+) (.+) (.+)/, (msg, match) => {
+bot.onText(/\/help(?: (.+))?/, (msg, match) => {
   const chatId = msg.chat.id;
-  const cmd = match[1]; 
-  const user = match[2];
+  const username =  msg.from.username;
 
-  const authenticated = checkAuth(user);
-  if (!authenticated) {
-    console.log('Not authenticated'); 
-    return;
-  } 
+  let helpMessage = `
+      Welcome to the Perezoso Telegram Bot help!
 
-  updateIdleState(user);
+      Available commands: `;
 
-  console.log(`Cmd is ${cmd} and user is ${user}`)
+  if (username && checkAuth(username)) {
+      // User is authenticated, show admin functions
+      helpMessage += `
+      /genCodes [number] [codeLength] [communityName] [yourUsername] [influencerAddress] - Generate codes
+      /authenticate [yourUsername] [token] - Authenticate user
+      /status [yourUsername] - Check user status`;
+  } else {
+      // User is not authenticated, show basic commands
+      helpMessage += `
+      /claim [code] [communityName] [recipientAddress] - Claim rewards`;
+  }
 
-  setLastCmd(cmd);
+  helpMessage += `
+      /help - Show this help message
 
+      Enjoy using the bot!`;
+
+  bot.sendMessage(chatId, helpMessage);
 });
 
 bot.onText(/\/genCodes (.+) (.+) (.+)/, (msg, match) => {
   const chatId = msg.chat.id;
-  const nCodes = match[1]; 
-  const codeLength = match[2];
-  const communityName = match[3];
+  const nCodes = parseInt(match[1]); 
+  const communityName = match[2];
+  const influencerAddress = match[3];
+  const user = msg.from.username;
 
-  if (isNaN(nCodes) || isNaN(codeLength)) {
-    bot.sendMessage(chatId, 'Invalid input. Please provide valid numbers for the number of codes and code length.');
+  // Validation for maximum number of codes
+  if (nCodes > 200) {
+    bot.sendMessage(chatId, 'You can only generate up to 200 codes at a time.');
     return;
   }
 
-  // const authenticated = checkAuth(resp);
-  // if (!authenticated) {
-  //   console.log('Not authenticated'); 
-  //   return;
-  // } 
+  // Basic input validation
+  if (isNaN(nCodes) || communityName === '' || influencerAddress === '') {
+    bot.sendMessage(chatId, 'Invalid input. Please provide valid data.');
+    return;
+  }
 
-  // updateIdleState(resp);
-
-  const codes = generateMultipleCodes(nCodes, codeLength);
-  console.log(`Generated codes: ${codes}`);
-
-  try{
-    saveCodesToJsonFile(codes, communityName);
+  // Authentication check
+  let authenticated = false;
+  try {
+    authenticated = checkAuth(user);
+    if (!authenticated) {
+      bot.sendMessage(chatId, `User ${user} is not authenticated.`);
+      return;
+    } 
   } catch (err) {
     bot.sendMessage(chatId, err.toString());
-    console.error("Error reading or parsing the file", err);
-   }
+    console.error("Error with authentication check", err);
+    return;
+  }
 
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, codes.join(', '));
+  // Updating the user's last active state
+  updateIdleState(user);
 
+  // Generate codes
+  const codes = generateMultipleCodes(nCodes, 24); // Assuming code length is fixed at 24 as per your example
+  console.log(`Generated codes: ${codes}`);
+
+  // Save codes to JSON file
+  try {
+    saveCodesToJsonFile(codes, communityName, influencerAddress);
+  } catch (err) {
+    bot.sendMessage(chatId, err.toString());
+    console.error("Error saving codes to JSON file", err);
+  }
+
+  // Format and send back the codes in a numbered list
+  const formattedCodes = codes.map((code, index) => `${index + 1}. ${code}`).join('\n');
+  bot.sendMessage(chatId, `Generated Codes:\n${formattedCodes}`);
 });
 
-bot.onText(/\/claim (.+) (.+)/, (msg, match) => {
+
+bot.onText(/\/claim (.+) (.+) (.+)/, async (msg, match) => {
   const chatId = msg.chat.id;
   const code = match[1]; 
   const communityName = match[2];
+  const recipientAddress = match[3];
+  const amount = '50000';
 
-  searchForCode(code, communityName);
+  console.log(`Attempting to transfer amount ${amount}`);
+  try {
+      const { found, foundIndex, influencerAddress } = await searchForCode(code, communityName);
 
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, code);
+      let tx
+      if (!found) {
+          bot.sendMessage(chatId, `Invalid code: ${code}`);
+      } else {
+          // Remove code from the list
+          await removeCodeFromList(communityName, foundIndex, influencerAddress);
+          
+          tx = await sendToken(recipientAddress, amount);
+          console.log(tx);
+          console.log(`Token sent successfully with tx hash: ${tx.hash}`);
+          bot.sendMessage(chatId, `Token sent successfully with tx hash: ${tx.hash}`);
 
-});
-
-bot.onText(/\/deposit (.+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const resp = match[1]; 
-  const username = resp;
-
-  const authenticated = checkAuth(username);
-  if (!authenticated) {
-    console.log('Not authenticated'); 
-    return;
-  } 
-
-  updateIdleState(username);
-
-  const token = userAuthData[username].token;
-  const key = `${username}:${token}`;
-
-  const addresses = depositAddresses[key];
-  console.log(addresses)
-
-  if (addresses) {
-    console.log(`Deposit address is ${addresses[0]}`)
-    // send back the matched "whatever" to the chat
-    bot.sendMessage(chatId, addresses[0]);
-  } else {
-      // Handle the case where the address is not found
-      bot.sendMessage(chatId, 'No address found');
+          tx = await sendToken(influencerAddress, amount);
+          console.log(tx);
+          console.log(`Token sent successfully with tx hash: ${tx.hash}`);
+      }
+  } catch (error) {
+      console.error("Error:", error);
+      bot.sendMessage(chatId, "An error occurred while processing your request.");
   }
-
 });
 
-bot.onText(/\/state (.+)/, (msg, match) => {   
+bot.onText(/\/authenticate/, (msg) => {
   const chatId = msg.chat.id;
-  const resp = match[1]; 
-
-  const authenticated = checkAuth(resp);
-  if (!authenticated) {
-    console.log('Not authenticated'); 
-    return;
-  } 
-
-  updateIdleState(resp);
-
-
-  if (typeof userStates[resp] !== 'undefined') {
-    // send back the matched "whatever" to the chat
-    bot.sendMessage(chatId, JSON.stringify(userStates[resp]));
-  }
-
-});
-
-bot.onText(/\/wagered (.+)/, (msg, match) => {    
-  const chatId = msg.chat.id;
-  const resp = match[1]; 
- 
-  const authenticated = checkAuth(resp);
-  if (!authenticated) {
-    console.log('Not authenticated'); 
-    return;
-  } 
-
-  updateIdleState(resp);
-
-  const userState = userStates[resp];
-  const wageredObj = {
-    wagered: userState.wagered,
-  }
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, JSON.stringify(wageredObj));
-
-});
-
-bot.onText(/\/profit (.+)/, (msg, match) => {    
-  const chatId = msg.chat.id;
-  const resp = match[1]; 
- 
-  const authenticated = checkAuth(resp);
-  if (!authenticated) {
-    console.log('Not authenticated'); 
-    return;
-  } 
-
-  updateIdleState(resp);
-
-  const userState = userStates[resp];
-  const profitObj = {
-    profit: userState.profit,
-  }
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, JSON.stringify(profitObj));
-
-});
-
-bot.onText(/\/authenticate (\w+) (\w+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const username = match[1];
-  const token = match[2];
-
-  let isAuthenticated = false;
-
-  // Create a key in the format used in depositAddresses
-  const userKey = `${username}:${token}`;
-
-  // Check if the key exists in depositAddresses
-  if (depositAddresses[userKey]) {
-      isAuthenticated = true;
-      // Update or add the user's auth data with authenticated flag set to true
-      userAuthData[username] = { token: token, idle: false, authenticated: true };
+  const username = msg.from.username;
+  console.log(depositAddresses)
+  // Check if the username exists in depositAddresses
+  if (depositAddresses[username]) {
+      // User is considered authenticated
+      userAuthData[username] = { authenticated: true, lastCommandTime: Date.now(), idle: false };
       bot.sendMessage(chatId, `User ${username} authenticated successfully.`);
-  }
-
-  if (!isAuthenticated) {
-      bot.sendMessage(chatId, "Invalid username or token.");
+  } else {
+      bot.sendMessage(chatId, `Authentication failed. No data found for this ${username}.`);
   }
 });
 
-bot.onText(/\/status (\w+)/, (msg, match) => {
+
+bot.onText(/\/status/, (msg) => {
   const chatId = msg.chat.id;
-  const username = match[1];
+  const username = msg.from.username; // Correctly accessing the username
   
   if (userAuthData[username]) {
       const userData = userAuthData[username];
       const status = {
           username: username,
           authenticated: userData.authenticated || false,
-          lastCmdOn: userData.lastCommandTime ? new Date(userData.lastCommandTime).toISOString() : 'N/A'
+          lastCmdOn: userData.lastCommandTime ? new Date(userData.lastCommandTime).toISOString() : 'N/A',
+          idle: userData.idle
       };
 
       bot.sendMessage(chatId, `Status: ${JSON.stringify(status, null, 2)}`);
   } else {
       bot.sendMessage(chatId, `No data found for username: ${username}`);
   }
-
-  if (userAuthData[username]) {
-    userAuthData[username].lastCommandTime = Date.now();
-    userAuthData[username].idle = false;
-  }
-
 });
+
 
 // Listen for any kind of message. There are different kinds of
 // messages.
